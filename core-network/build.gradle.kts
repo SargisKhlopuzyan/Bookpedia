49010ac91d5f9b8c4e8af18186bfb73db85f67a1
Sargis Khlopuzyan <sargis.khlopuzyan@ingenico.com>
2026-03-13 11:02:50 +0400

    Implemented core-network di

--- core-network/build.gradle.kts
+++ core-network/build.gradle.kts
@@ -31,9 +31,12 @@ kotlin {
             implementation(libs.ktor.client.android)
         }
         commonMain.dependencies {
+            implementation(libs.kotlinx.serialization)
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.serialization)
+
+            implementation(libs.koin.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
